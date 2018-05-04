575621e0e7301ee96f9c57442c34cb02a076a067
Soohwan.Cho <chh0107@gmail.com>
2018-05-04 16:58:14 +0900

    fixed untracked files

--- Pods/Target Support Files/Pods-Sandart/Pods-Sandart-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Sandart : NSObject
-@end
-@implementation PodsDummy_Pods_Sandart
-@end
--- Pods/Target Support Files/Pods-SandartTests/Pods-SandartTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SandartTests : NSObject
-@end
-@implementation PodsDummy_Pods_SandartTests
-@end
